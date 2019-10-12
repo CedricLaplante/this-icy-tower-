e69419ff54b4b65c1ed5e931e647f027f53c6a6c
CedricLaplante <cedballou26@icloud.com>
2019-10-11 23:52:16 -0400

    tp last version

--- cours3/Assets/script/Jump.cs
+++ cours3/Assets/script/Jump.cs
@@ -4,50 +4,23 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    public Rigidbody m_RigidBody;
-    public Vector3 m_Force = new Vector3();
+    public Rigidbody rb;
+    public float JumpForce = 7f;
+    public SphereCollider col;
+    public LayerMask groundLayer;
 
     void Start()
     {
-       
+       col = GetComponent<SphereCollider>();
     }
-
-    private bool m_IsOnGround;
-    
     // Update is called once per frame
     void Update()
     {
-        if (m_IsOnGround && Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            m_RigidBody.AddForce(m_Force);
+             rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse); 
         }
-
-        RaycastHit hit;
-        // Vector3.up == new Vector3(of, 1f, 0f)
-        // -Vector3.up == new Vector(0f, -1f, 0f)
-        // si Mon raycast est vrai, mon raycast a toucher un collider, si mon raycast est faux, mon raycast n'a pas toucher de collider 
-
-        // Vector3 v1 = new Vector3(0f, 1f, 0f);
-        // Vector3 v2 = new Vector3(1f, 0f, 1f);
-        // Vector3 v3 = v1 + v2; // v3.x = v1.x + v2.x // v3.y = v1.y + v2.y
-
-        //On décale le raycast de 0.1f en y pour que le raycast soit lancer un peu plus haut que la collision de la plateforme
-        Vector3 raycastOrigin = new Vector3(0.5f, 0f, 0f);
-        
-        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 0.2f))            
-        {
-            // J'AI TOUCHER UN COLLIDER
-            m_IsOnGround = true;
-            Debug.Log("TOUCHE");
-        }
-        else
-        {
-            // J'AI PAS TOUCHER AU COLLIDER
-            m_IsOnGround = false;
-            Debug.Log("TOUHCE PAS");
-        }
-
-        Debug.DrawRay(transform.position + new Vector3(0f, 0.1f, 0f), -Vector3.up * 0.2f, Color.black);
-
     }
+    
+   
 }
--- cours3/Assets/script/TranslateObject.cs
+++ cours3/Assets/script/TranslateObject.cs
@@ -13,8 +13,6 @@ public class TranslateObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Time.DeltaTime = durée entre le frame présent et le dernier frame
-        // donc 60 fps, time.DeltaTime = 1 / 60 = 0.016666666666;
-        transform.Translate(m_Direction * Time.deltaTime); // bouger dans la direction à tout les secondes.
+        transform.Translate(m_Direction * Time.deltaTime);  
     }
 }
